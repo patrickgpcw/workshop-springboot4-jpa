46faae52b442f1fc44b97d6102281f64e25c31f5
patrickpessanha <patrick.surfista@hotmail.com>
2026-09-24 15:04:12 -0400

    first commit

---
+++ src/test/java/com/patrick/workshopspringboot4jpa/WorkshopSpringboot4JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.workshopspringboot4jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot4JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
